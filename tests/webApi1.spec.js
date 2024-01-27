357b73ca47b4d0f018b1e6364ccdd26e3d733d3a
bhushantbn <bhushantbn@gmail.com>
2024-01-27 20:31:03 +0530

    added webapi testcases

--- tests/webApi1.spec.js
+++ tests/webApi1.spec.js
@@ -28,5 +28,19 @@ test('@Client App login', async ({ page }) => {
    await page.locator("text=Checkout").click();
  
    await page.locator("[placeholder*='Country']").fill("ind");
-   
+   const dropdown = page.locator(".ta-results");
+   await dropdown.waitFor();
+   const optionsCount = await dropdown.locator("button").count();
+   for (let i = 0; i < optionsCount; ++i) {
+      const text = await dropdown.locator("button").nth(i).textContent();
+      if (text === " India") {
+         await dropdown.locator("button").nth(i).click();
+         break;
+      }
+   }
+   expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+   await page.locator(".action__submit").click();
+   await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+   const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+   console.log(orderId);
 })
