fa39426eac5c56e3d36454d00f57f85c35c533f2
bhushantbn <bhushantbn@gmail.com>
2024-02-01 08:12:35 +0530

    added page object model testcases

---
+++ tests/Pomtest.spec.js
@@ -0,0 +1,31 @@
+import {test,expect, chromium} from "@playwright/test"
+import { LoginPage } from "../pages/LoginPage"
+import { CartPage } from "../pages/CartPage";
+import { HomePage } from "../pages/HomePage";
+
+test("Login Page object model",async({page})=>{
+
+    let browser=await chromium.launch()
+
+    //LoginPage
+    const login=new LoginPage(page);
+    await login.gotoLoginPage();
+    await login.login('pavanol','test@123');
+    await page.waitForTimeout(3000);
+
+    //HomePage
+    const home=new HomePage(page);
+    await home.addProductToCart('Nexus 6')
+    await page.waitForTimeout(3000)
+    await home.gotoCart()
+
+    // //CartPage
+    const cartPage=new CartPage(page)
+    await page.waitForTimeout(3000)
+    const status=await cartPage.checkProductInCart('Nexus 6')
+    expect(await status).toBe(true)
+    browser.close()
+    page.close()
+
+    
+})
