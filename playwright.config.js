b361e308daba5f0495694968c8ce4b251cafe711
bhushantbn <bhushantbn@gmail.com>
2024-02-10 16:16:37 +0530

    release all test

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["dot"],["json",{outputFile:"test-result.json"}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -32,14 +32,12 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless:false,
+    headless:true,
     channel:"chrome"
    
    
   },
-  
-  
-
+  testMatch:["**.test.js"],
   /* Configure projects for major browsers */
   projects: [
     {
